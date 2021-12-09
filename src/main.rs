c17cb3747b9b5bd1d31b017091af47008901c802
BuildTools <unconfigured@null.spigotmc.org>
2021-12-09 23:31:47 +0900

    7-5 subscription

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,75 @@
 use iced::{
-    button, executor, Align, Application, Button, Column, Command, Element, Font,
-    HorizontalAlignment, Length, Row, Settings, Subscription, Text,Clipboard
+    button, executor, Align, Application, Button, Clipboard, Column, Command, Element, Font,
+    HorizontalAlignment, Length, Row, Settings, Subscription, Text,
 };
+use iced_futures::{self,futures};
+use std::time::{Duration,Instant};
 
 const FONT: Font = Font::External {
     name: "PixelMplus12-Regular",
     bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
 };
 
+pub struct Timer {
+    duration: Duration,
+}
+
+impl Timer {
+    fn new(duration: Duration) -> Timer {
+        Timer { duration: duration }
+    }
+}
+
+impl<H, E> Subscription::Recipe<H, E> for Timer
+where
+    H: std::hash::Hasher,
+{
+    type Output = instant;
+    fn hash(&self, state: &mut H) {
+        use std::hash::Hash;
+        std::any::TypeId::of::<Self>().hash(state);
+        self.duration.hash(state);
+    }
+
+    fn stream(
+        self: Box<Self>,
+        _input: futures::stream::BoxStream<'static, E>,
+    ) -> futures::stream::BoxStream<'static, Self::Output> {
+        use futures::stream::StreamExt;
+        async_std::stream::interval(self.duration)
+            .map(|_| Instant::now())
+            .boxed()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    Start,
+    Stop,
+    Reset,
+    Update,
+}
+
+pub enum TickState {
+    Stopped,
+    Ticking,
+}
+
 struct GUI {
+    tick_state: TickState,
     start_stop_button_state: button::State,
     reset_button_state: button::State,
 }
 
 impl Application for GUI {
     type Executor = executor::Default;
-    type Message = ();
+    type Message = Message;
     type Flags = ();
 
     fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
         (
             GUI {
+                tick_state: TickState::Stopped,
                 start_stop_button_state: button::State::new(),
                 reset_button_state: button::State::new(),
             },
@@ -29,42 +78,69 @@ impl Application for GUI {
     }
 
     fn title(&self) -> String {
-        String::from("DEMO")
+        String::from("Stopwatch")
     }
 
     fn update(
         &mut self,
-        _message: Self::Message,
+        message: Self::Message,
         _clipboard: &mut Clipboard,
     ) -> Command<Self::Message> {
+        match message {
+            Message::Start => {
+                self.tick_state = TickState::Ticking;
+            }
+            Message::Stop => {
+                self.tick_state = TickState::Stopped;
+            }
+            Message::Reset => {}
+        }
         Command::none()
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-       //init widgets
-       let tick_text = Text::new("00:00:00.00").font(FONT).size(60);
-       let start_stop_button = Button::new(
-           &mut self.start_stop_button_state,
-           Text::new("Start")
-                .horizontal_alignment(HorizontalAlignment::Center)
-                .font(FONT),
-       )
-       .min_width(80);
-       let reset_button = Button::new(
-           &mut self.reset_button_state,
-           Text::new("Reset")
-                .horizontal_alignment(HorizontalAlignment::Center)
-                .font(FONT),
-       )
-       .min_width(80);
+        //prepare duration text
+        let duration_text = "00:00:00.00";
 
-       Column::new()
+        //prepare start/stop text
+        let start_stop_text = match self.tick_state {
+            TickState::Stopped => Text::new("Start")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+            TickState::Ticking => Text::new("Stop")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        };
+
+        //prepare start/sop message on button press
+        let start_stop_message = match self.tick_state {
+            TickState::Stopped => Message::Start,
+            TickState::Ticking => Message::Stop,
+        };
+
+        //prepare duration text
+        let tick_text = Text::new(duration_text).font(FONT).size(60);
+        //init widgets
+        let start_stop_button = Button::new(&mut self.start_stop_button_state, start_stop_text)
+            .min_width(80)
+            .on_press(start_stop_message);
+
+        let reset_button = Button::new(
+            &mut self.reset_button_state,
+            Text::new("Reset")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80)
+        .on_press(Message::Reset);
+
+        Column::new()
             .push(tick_text)
             .push(
                 Row::new()
-                        .push(start_stop_button)
-                        .push(reset_button)
-                        .spacing(10),
+                    .push(start_stop_button)
+                    .push(reset_button)
+                    .spacing(10),
             )
             .spacing(10)
             .padding(10)
@@ -77,6 +153,6 @@ impl Application for GUI {
 
 fn main() {
     let mut settings = Settings::default();
-    settings.window.size = (400u32,120u32);
+    settings.window.size = (400u32, 120u32);
     GUI::run(settings);
 }
